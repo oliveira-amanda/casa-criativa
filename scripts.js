a3f117e2c417b1a4e316cc385adc58c628b720a1
amanda-oliveira <amanda.bap.oliveira@gmail.com>
2020-03-30 17:04:26 -0300

    Create Ideas page

--- scripts.js
+++ scripts.js
@@ -3,4 +3,12 @@ function onOff(){
         .querySelector("#modal")
         .classList
         .toggle("hide")
+    document    
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+    document 
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
 }
